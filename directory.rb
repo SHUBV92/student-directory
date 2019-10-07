79cd9d94ab4aa68494ac7c18f88ebaea0cf037a4
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-07 19:44:35 +0100

    Added text

---
+++ directory.rb
@@ -0,0 +1,18 @@
+puts "The students of Villains Academy"
+puts "_ _ _ _ _ _ _ _"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffery Baratheon"
+puts "Norman Bates"
+
+
+print "Overall, we have"
+print 11
+puts " great students"
