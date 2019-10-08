671db8d6627fffcc7d20f110bf2fa2f1a4fff67d
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-08 13:46:46 +0100

    Init Refactoring

---
+++ directory_copy.rb
@@ -0,0 +1,72 @@
+#let's put all students into an array
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Krueger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Joffery Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+# ]
+
+# def print_header
+# puts "The students of Villains Academy"
+# puts "_ _ _ _ _ _ _ _"
+# end 
+
+# def print(students)
+#     students.each do |student|
+#         puts "#{student[:name]} (#{student[:cohort]}cohort)"
+#     end
+# end
+
+# def print_footer(names)
+# puts "Overall, we have #{names.count} great students"
+# end 
+
+# print_header
+# print(students)
+# print_footer(students)
+
+
+
+def input_students 
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+
+    students = []
+
+    name = gets.chomp
+
+    while !name.empty? do 
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+
+        name = gets.chomp
+    end 
+
+   
+    end
+    end
+
+    students
+
+end 
+
+
+# print_header
+print(students)
+# print_footer(students)
+
+# methods used to modify the program
+# 1: each ()
+# 2: each_with_index
+# 3: print students who's name starts wit a specific letter 
+# 4: while or until (Loops)
+# 5: Add more keys to your hashes 
+# 6: Research how the method center() of string class work  
+# 7: 
