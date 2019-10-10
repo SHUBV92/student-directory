e76bf3b397046427a55d47fde4247bef6dfa72ef
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-10 17:43:18 +0100

    Chapter 8.9

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,30 +1,26 @@
-# Q:10 We've been using the chomp() method to get rid of the last return character. 
-# Find another method among those provided by the String class that could be used 
-# for the same purpose (although it will require passing some arguments).
+# Q9 ------------------------------------------------------
 
-
-
-
+# Right now if we have only one student, the user will see a message "Now we have 1 students", 
+# whereas it should be "Now we have 1 student". How can you fix it so that it used singular form 
+# when appropriate and plural form otherwise?
 
 def input_students 
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
 
     students = []
-    name = gets.chop 
-    # .chomp has been replaced with .chop a method provided by the string class 
-    #  that has the same function
-
-    while !name.empty? do 
-        students << {name: name, cohort: :november}
-         puts "Now we have #{students.count} students"
-         name = gets.chop
+    name = gets.chomp
+  
+    while !name.empty? do         
+        students << {name: name, cohort: :November}
+        if students.count == 1
+        puts "Now we have #{students.count} student"
+        else   
+            puts "Now we have #{students.count} students"     
+        end   
+        name = gets.chomp    
     end 
-     return students
+    return students
 end
 
 input_students
-
-
-
-
