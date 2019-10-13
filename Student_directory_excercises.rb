7cab81d6a7085d7f3f235453dfbd4c420dfb87e7
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-10-13 22:32:23 +0100

    Chapter 8 Ex: 6

--- Student_directory_excercises.rb
+++ Student_directory_excercises.rb
@@ -1,6 +1,6 @@
 def input_students 
-  puts "Please enter students information"
-  puts "To finish, just hit return four times"
+  puts "Please enter students information".center(20, " ")
+  puts "To finish, just hit return four times".center(20, " ")
 
   @students = []
   name = gets.chomp
@@ -11,7 +11,7 @@ def input_students
   while !name.empty? do 
     @students << {name: name, hobbies: hobbies, country_of_birth: country_of_birth, 
     height: height, cohort: :november}
-    puts "Now we have #{@students.count} students"
+    puts "Now we have #{@students.count} students".center(20, " ")
     name = gets.chomp
     hobbies = gets.chomp
     country_of_birth = gets.chomp
@@ -22,13 +22,13 @@ end
 
 def print_students
   @students.each_with_index { |student, index| 
-  puts "#{index+1} #{student[:name]} #{student[:hobbies]} #{student[:country_of_birth]} #{student[:height]} (#{student[:cohort]} cohort)"}              
+  puts "#{index+1} #{student[:name]} #{student[:hobbies]} #{student[:country_of_birth]} #{student[:height]} (#{student[:cohort]} cohort)"}.center(20, " ")              
 end
 
 def sorted_by_alphabet
   index = 1
   @students.each {|x| if x[:name].start_with? ("a") && x[:cohort] == "november".to_sym && x[:name].length <= 12
-  puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)"
+  puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)".center(20, " ")
   index += 1}
 end
   
